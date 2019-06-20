1ddb00de641243ee394da585c178a308432f4dd5
Filip-Ante Kovacic <filip.ante.kovacic@tmc-solutions.xyz>
2019-06-20 21:47:30 +0200

    Add mail controller

--- sync/main.go
+++ sync/main.go
@@ -1,9 +1,82 @@
 package main
 
-import "fmt"
+import (
+	"context"
+	"os"
+	"time"
+
+	"github.com/globalsign/mgo/bson"
+	"golang.org/x/oauth2"
+)
 
 func main() {
 
-	fmt.Println("aa")
+	Sync()
+
+}
+
+// Sync get users & start sync emails
+func Sync() {
+
+	users := GetUsers()
+
+	if len(users) != 0 {
+
+		for _, user := range users {
+
+			BackupMails(user)
+
+		}
+
+	}
+
+}
+
+// BackupMails save emails to db
+func BackupMails(user User) {
+
+	client := user.Config.Client(context.Background(), user.Token)
+
+	gmailMain(user, client)
+
+}
+
+// User struct in DB
+type User struct {
+	ID          bson.ObjectId  `json:"id" bson:"_id,omitempty"`
+	Email       string         `json:"email" bson:"email,omitempty"`
+	Password    string         `json:"password" bson:"password,omitempty"`
+	Credentials []byte         `json:"credentials" bson:"credentials,omitempty"`
+	Config      *oauth2.Config `json:"config" bson:"config,omitempty"`
+	Token       *oauth2.Token  `json:"token" bson:"token,omitempty"`
+	Created     time.Time      `json:"created" bson:"created,omitempty"`
+	Modified    time.Time      `json:"modified" bson:"modified,omitempty"`
+}
+
+// GetUsers return array users from db
+func GetUsers() []User {
+
+	proc := ServiceLog{
+		Start:   time.Now(),
+		Type:    "Function",
+		Service: "user",
+		Name:    "GetUsers",
+	}
+
+	defer SaveLog(proc)
+
+	var rows []User
+
+	DB := MongoSession()
+	DBC := DB.DB(os.Getenv("MONGO_DB")).C("users")
+	defer DB.Close()
+
+	err := DBC.Find(bson.M{}).Select(bson.M{"password": 0}).All(&rows)
+	if err != nil {
+		HandleError(proc, "get users", err, true)
+		return rows
+	}
+
+	return rows
 
 }
